c211c245f09d8335a520cd7a0b5360897d4988eb
schneems <richard.schneeman+foo@gmail.com>
2019-12-03 15:50:50 -0600

    Remove script

--- vendor_libpq.sh
+++
@@ -1,5 +0,0 @@
-cd vendor
-curl --remote-name-all https://apt.postgresql.org/pub/repos/apt/pool/main/p/postgresql-12/libpq5_12.1-1.pgdg16.04+1_amd64.deb
-dpkg -x libpq5_12.1-1.pgdg16.04+1_amd64.deb .
-cd ..
-export LD_LIBRARY_PATH="$HOME/vendor/usr/lib/x86_64-linux-gnu/:$LD_LIBRARY_PATH"
